8abed71351eb4fdad03e46c17a0adb7ac22b0dd5
Sergey Kukunin <sergey.kukunin@gmail.com>
2016-03-20 11:59:20 +0200

    Set unique cookie for every client

--- libs/amiAlone.js
+++ libs/amiAlone.js
@@ -1,5 +1,21 @@
+var crypto = require('crypto');
+
 module.exports = function(logger, portalConfig, poolConfigs){
+    var COOKIE_NAME = '_worker_id';
+    var ID_LENGTH = 32;
+
+    var ensureWorkerIdCookie = function(req, res) {
+        var cookie = req.cookies[COOKIE_NAME];
+        if (cookie === undefined)
+        {
+            cookie = crypto.randomBytes(ID_LENGTH).toString('hex');
+            res.cookie(COOKIE_NAME, cookie, {maxAge: 900000, httpOnly: true, path: '/'});
+        }
+        return cookie;
+    }
+
     this.handleRequest = function(req, res, next){
-        res.end("Hello World");
+        var cookie = ensureWorkerIdCookie(req, res);
+        res.end("Your cookie is " + cookie);
     };
 };
